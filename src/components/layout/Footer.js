73846bc3537a5c3beb92be7a5f581588d7976240
green-cats <62088807+green-cats@users.noreply.github.com>
2022-05-06 22:06:25 +0600

    design issue

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,20 +1,20 @@
 const Footer = () => {
   return (
     <div className='footer-container fixed left-0 right-0 bottom-0'>
-      <footer className='text-[12px] border-t border-[rgba(225,225,225,.1)] text-gray-400 sm:max-w-[35rem] lg:max-w-[100rem] flex flex-wrap basis-full items-center w-full mx-auto py-5'>
-        <div>
-          <span className='uppercase'>Nftcally</span> 2022 | All Rights Reserved
+      <footer className='border-t border-[rgba(225,225,225,.1)] text-gray-400 sm:max-w-[35rem] lg:max-w-[100rem] flex flex-wrap basis-full items-center w-full mx-auto py-5 px-5'>
+        <div className='text-center block w-full lg:w-auto lg:flex items-center lg:mx-[unset] text-[12px] mb-[10px] lg:mb-0'>
+          <span className='uppercase mr-2'>Nftcally</span> 2022 | All Rights Reserved
         </div>
 
-        <nav className='ml-auto'>
+        <nav className='mx-auto lg:mx-[unset] lg:ml-auto text-[10px] md:text-[12px]'>
           <ul className='flex'>
-            <li className='mr-8'>
+            <li className='mr-2 sm:mr-8'>
               <a href='/'>Community Guidelines</a>
             </li>
-            <li className='mr-8'>
+            <li className='mr-2 sm:mr-8'>
               <a href='/'>Risk Policy</a>
             </li>
-            <li className='mr-8'>
+            <li className='mr-2 sm:mr-8'>
               <a href='/'>Terms</a>
             </li>
             <li>
--- src/components/layout/HeaderMenu.js
+++ src/components/layout/HeaderMenu.js
@@ -5,11 +5,11 @@ import AsideLeft from "../aisde/AsideLeft";
 export function HeaderMenu() {
   return (
     <header className='absolute w-full top-0 left-0 py-10 z-[100]'>
-      <nav className='sm:max-w-[90rem] 2xl:max-w-[105rem] flex flex-wrap basis-full items-end w-full mx-auto px-4 sm:px-6 lg:px-8'>
+      <nav className='sm:max-w-[90rem] 2xl:max-w-[105rem] flex basis-full items-center sm:items-end w-full mx-auto px-4 sm:px-6 lg:px-8'>
         <AsideLeft />
 
-        <a className='flex-none' href='/'>
-          <img src={logo} alt='logo' />
+        <a className='max-w-[250px] sm:max-w-[unset] flex-none' href='/'>
+          <img className='block' src={logo} alt='logo' />
         </a>
       </nav>
     </header>
