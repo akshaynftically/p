85db1565b5ddd17ac82e88a66ef61f437633c530
o2sevruk <o2sevruk@gmail.com>
2022-05-09 21:44:03 +0300

    landing is in progress.

--- src/views/Promo/sections/Fourth.js
+++ src/views/Promo/sections/Fourth.js
@@ -1,5 +1,15 @@
 const Fourth = () => {
-  return <div>Fourth</div>
+  return (
+    <div className='bg-[#161718] py-[70px]'>
+      <div className='max-w-[1340px] mx-auto px-4 lg:px-8'>
+        <h2 className='leading-2 font-extrabold text-center text-[32px] lg:text-[48px] lg:mb-[200px]'>
+          <span className='text-gradient'>A Metaverse</span>
+          <br />
+          That Users Will Flock To
+        </h2>
+      </div>
+    </div>
+  )
 }
 
 export default Fourth
