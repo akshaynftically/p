3aa17560fa5602b7b326b5fb5824d1a6549dad75
akshay tiwari <akshay tiwari>
2022-05-30 12:25:55 +0530

    remove colon from label

--- src/components/form/FieldGroup.js
+++ src/components/form/FieldGroup.js
@@ -5,7 +5,7 @@ const FieldGroup = (props) => {
 
   return (
     <div className={clsx('mb-[16px] md:mb-[24px]', className)}>
-      {label && <label className='block text-[14px] text-white/[.80] mb-[6px]'>{label} {required && <span style={{color:'red'}}>*</span>}:</label>}
+      {label && <label className='block text-[14px] text-white/[.80] mb-[6px]'>{label} {required && <span style={{color:'red'}}>*</span>}</label>}
       {children}
     </div>
   )
