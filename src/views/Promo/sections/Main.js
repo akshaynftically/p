e963c9658e03d119a897d2c44dfe43e640fdcab3
akshay <akshay>
2022-06-02 00:50:54 +0530

    change env value

--- src/views/Promo/sections/Main.js
+++ src/views/Promo/sections/Main.js
@@ -21,7 +21,7 @@ import ProgressConnectYourWallet from 'modals/ProgressConnectYourWallet'
 
 
 const Main = () => {
-  const isTimer=!(process.env.REACT_APP_IS_MAINNET_ENABLED == 'false')                    //converting  env data string into boolean
+  const isTimer=!(process.env.REACT_APP_IS_TIMMER == 'false')                    
   const modelViewerRef = useRef()
   const navigate = useNavigate()
   const [isOpenedProgressWallet, setIsOpenedProgressWallet] = useState(false)
