a7683180a44a7d077986536dc206c04c103b89e6
KuldeepNftically <kuldeep.soni@nftically.com>
2022-06-28 13:04:37 +0530

    Duplicate meta descriptions

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -7,6 +7,7 @@ const About = () => {
     <>
     <div className='bg-[#161718] text-white lg:pb-[80px]'>
     <Helmet>
+    <title>About COMEARTH - Web3.0 E-Commerce Metaverse & Ecosystem</title>
     <link rel="canonical" href="https://www.comearth.world/about-us" />
     </Helmet>
       <Main />
